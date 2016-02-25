0318f5640cb469f7b947e91e1a409e918c714e39
mpdietz <mpd361@nyu.edu>
2016-02-25 16:38:05 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-var map = L.map('myMap').setView([40.7127837, -74.0059413], 9);
+// Add mapp
+var basemapUrl = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+  var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
 
-var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+  //initialize map1
+  var map1 = L.map('map1', {
+    scrollWheelZoom: false
+  }).setView( [40.706913,-73.987513], 1);
+
+var layer = L.tileLayer(basemapUrl, {
+  attribution: attribution
 });
 
 map.addLayer(layer)
+
+  $.getJSON('data/world-countries.geojson', function(data) {
+    console.log(data);
 
   galleryimages.forEach(function(element) {
     var marker = L.marker(element.geolocation).addTo(map);
@@ -15,4 +25,54 @@ map.addLayer(layer)
  var panOptions = {
     animate: true,
     duration: 2
+  }
+
+  // From Chloropleth tutorial
+
+var geojson;
+
+  //this function takes a value and returns a color based on which bucket the value falls between
+  function getColor(d) {
+      return d > 1000 ? '#0000cc' :
+             d > 500  ? '#BD0026' :
+             d > 200  ? '#E31A1C' :
+             d > 100  ? '#FC4E2A' :
+             d > 50   ? '#FD8D3C' :
+             d > 20   ? '#FEB24C' :
+             d > 10   ? '#FED976' :
+                        '#FFEDA0';
+  }
+
+  //this function returns a style object, but dynamically sets fillColor based on the data
+  function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+  }
+
+  //this function is set to run when a user mouses over any polygon
+  function mouseoverFunction(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+
+
+// Update the infowindow
+      console.log(layer.feature.properties.name);
+    $('#infoWindow').text(layer.feature.properties.name);
+
   }
